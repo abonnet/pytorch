7f286e9b3e3ad861cd03e1f4d204f75a4383fe8c
Jerry Zhang <jerryzh168@gmail.com>
2019-09-24 12:12:51 -0700

    [fix] quantized_tensor tests
    Summary:
    Previously we are using empty to generate test tensors, this PR changes the test tensors to use
    randint so that we can test things properly
    Also added a set_sizes_and_strides and removed .contiguous() in int_repr function to preserve the
    original size and strides
    
    Test Plan:
    python test/test_quantized_tensor.py
    
    Reviewers:
    pt1quant
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: abef33f0e254b6cf520a1325b9f85f841af0a578
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/25429

--- aten/src/ATen/native/quantized/QTensor.cpp
+++ aten/src/ATen/native/quantized/QTensor.cpp
@@ -1,5 +1,7 @@
 #include <ATen/ATen.h>
 #include <ATen/NativeFunctions.h>
+#include <ATen/native/TensorIterator.h>
+#include <ATen/native/cpu/Loops.h>
 #include <ATen/quantized/QTensorImpl.h>
 #include <ATen/quantized/Quantizer.h>
 
@@ -102,16 +104,17 @@ IntArrayRef q_per_channel_axis_quant(const Tensor& self) {
 
 Tensor int_repr_quant(const Tensor& self) {
   Tensor dst;
-  // TODO: replace with TensorIterator
-  auto self_c = self.contiguous();
   AT_DISPATCH_QINT_TYPES(self.scalar_type(), "int_repr", [&]() {
     dst = at::empty(self.sizes(), self.options().dtype(UNDERLYING_TYPE));
-    underlying_t* self_data =
-        reinterpret_cast<underlying_t*>(self_c.data_ptr<scalar_t>());
-    underlying_t* dst_data = dst.data_ptr<underlying_t>();
-    if (self.numel() > 0) {
-      memcpy(dst_data, self_data, self.nbytes());
-    }
+    dst.unsafeGetTensorImpl()->set_sizes_and_strides(self.sizes(), self.strides());
+    auto iter = TensorIterator();
+    iter.add_output(dst);
+    iter.add_input(self);
+    iter.dont_compute_common_dtype();
+    iter.build();
+    cpu_kernel(iter, [&](scalar_t value) -> underlying_t {
+       return value.val_;
+    });
   });
   return dst;
 }
